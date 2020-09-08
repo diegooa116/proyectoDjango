fe83bcb622ee0be5f27bc10417a9f06e59fadfad
diegooa116 <61633319+diegooa116@users.noreply.github.com>
2020-09-08 12:19:13 -0500

    Initial commit

---
+++ primeraPruebaASI/GYM/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+#Api rest imports
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from GYM.serializers import UserSerializer, GroupSerializer
+
+# Create your views here.
+
+def saludo(request):
+    
+    return HttpResponse("Hola mundo en Django...")
+
+
+
+def despedida(request):
+    
+    return HttpResponse("Adios a todos...")
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
